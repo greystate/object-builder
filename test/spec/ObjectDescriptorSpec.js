d788b9b45f5bb0f350f0eb12fbb217b9c3d21b32
Chriztian Steinmeier <chriztian@steinmeier.dk>
2020-04-26 17:13:08 +0200

    Write specs for `addMethod` and `addProperty`

---
+++ test/spec/ObjectDescriptorSpec.js
@@ -0,0 +1,49 @@
+describe("ObjectDescriptor", () => {
+	beforeEach(() => {
+		this.od = new ObjectDescriptor()
+		this.point = new ObjectDescriptor("Point")
+		this.point.addProperty("x")
+		this.point.addProperty("y")
+		this.point.addMethod("draw")
+		this.point.addMethod("toString")
+	})
+	
+	it("has a name", () => {
+		expect(new ObjectDescriptor("Ufo").name).toEqual("Ufo")
+	})
+	
+	describe("addMethod", () => {
+		it("adds a method to the ObjectDescriptor", () => {
+			this.od.addMethod("toString")
+			expect(this.od.methods.length).toEqual(1)
+			expect(this.od.methods[0].name).toEqual("toString")
+		})
+	})
+	
+	describe("addProperty", () => {
+		it("adds a property to the ObjectDescriptor", () => {
+			this.od.addProperty("length")
+			expect(this.od.properties.length).toEqual(1)
+			expect(this.od.properties[0].name).toEqual("length")
+		})
+		
+		it("sets the type to 'default' if none specified", () => {
+			this.od.addProperty("isDefault")
+			expect(this.od.properties[0].type).toEqual("default")
+		})
+		
+		it("extracts the type when specified", () => {
+			this.od.addProperty("isBoolean?")
+			this.od.addProperty("isArray[]")
+			this.od.addProperty("isDictionary{}")
+			this.od.addProperty("isInt:int")
+			this.od.addProperty("isDouble:double")
+			expect(this.od.properties[0].type).toEqual("boolean")
+			expect(this.od.properties[1].type).toEqual("array")
+			expect(this.od.properties[2].type).toEqual("dictionary")
+			expect(this.od.properties[3].type).toEqual("integer")
+			expect(this.od.properties[4].type).toEqual("double")
+		})
+	})
+	
+})
